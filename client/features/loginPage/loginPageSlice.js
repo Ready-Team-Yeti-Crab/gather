691638337777a39af832f305dfdeb247f488df22
Anthony Deng <anthonyadeng@gmail.com>
2023-03-11 15:19:55 -0500

    worked on login page slice

--- client/features/loginPage/loginPageSlice.js
+++ client/features/loginPage/loginPageSlice.js
@@ -9,29 +9,33 @@ export const loginPageSlice = createSlice({
 	initialState,
 	reducers: {
 		register: (state, action) => {
-			const { username, password } = action.payload;
 			//make fetch call passing in username and password
-
+			fetch('/signup', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(action.payload),
+			});
 			//update alert state
-			//
 
 			// Redux Toolkit allows us to write "mutating" logic in reducers. It
 			// doesn't actually mutate the state because it uses the Immer library,
 			// which detects changes to a "draft state" and produces a brand new
 			// immutable state based off those changes
-			state.value += 1;
+
 			// action === {type: 'register', payload: {username: username, password: password}}
 		},
 		login: (state, action) => {
-			state.value -= 1;
+			//CONFIRM WITH BACKEND REGARDING EXISING USER LOGIN ENDPOINT
+			fetch('/login', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(action.payload),
+			});
 		},
-		// incrementByAmount: (state, action) => {
-		// 	state.value += action.payload;
-		// },
 	},
 });
 
 // Action creators are generated for each case reducer function
-export const { register, decrement } = loginPageSlice.actions;
+export const { register, login } = loginPageSlice.actions;
 
 export default loginPageSlice.reducer;
