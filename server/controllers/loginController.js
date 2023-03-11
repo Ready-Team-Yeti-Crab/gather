176f472c1690f093ae9b2fae54ac75bb0ed354f3
victoriadillman <victoria.dillman@gmail.com>
2023-03-11 13:55:56 -0500

    authorization, hashing, and bcrytping to database on sign in working

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,21 +1,50 @@
 // serve in the MongoDB database
-const User = require('../model/models.js')
+const {User} = require('../model/models.js')
 const bcrypt = require('bcryptjs');
 
 const loginController = {}
 
+// Create User controller (Variables: username & password)
 loginController.createUser = (req, res, next) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
   if (username && password) {
     User.create({username: username, password: password})
-      .then(() => {
-        // do a find request of user and store the userId onto our res.locals (for our cookies)
-      })
-      .catch((err) => {
+      .then((() => {
+        User.find({username: username})
+          .then((data) => {
+            res.locals.userId = data[0]._id;
+            return next();
+          })
+          .catch((err) => {
+            return next({
+              log: `Error in retrieving the newly created user in createUser ${err}`,
+              status: 500,
+              message: { err: 'An error occurred' },
+            })
+          })
+      }))
+      .catch((err => {
         return next({
-          // Global error handler here
+          log: `Error in creation of user ${err}`,
+          status: 500,
+          message: { err: 'An error occurred' },
         })
-      })
+      }))
   }
-  // Else redirect back to signup or send error code? Ask the team 
+  else {
+    res.redirect(400, '/signup');
+  }
 }
+
+// Setting session cookie
+
+
+// Compare password middleware with salt VIC: CHECK THE LOGIC HERE ON VARIABLE NAMES
+loginController.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err)
+    cb(null, isMatch)
+  })
+}
+
+module.exports = loginController;
