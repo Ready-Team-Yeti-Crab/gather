dec22727758a67c5f7d4dd291d7fbbdf07f4c147
sffw <spencerffwhite@gmail.com>
2023-03-10 20:26:07 -0500

    mongodb connected

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
+const mongoose = require('mongoose')
 const path = require("path");
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const MONGO_URI = 'mongodb+srv://Stan:TGJQJ2YK8pTgZjwC@gathercluster.59lrgnt.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.connect(MONGO_URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // sets the name of the DB that our collections are part of
+  dbName: 'GatherDB'
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
 
 // SERVE STATIC AND PARSERS
 if (process.env.NODE_ENV === "production") {
