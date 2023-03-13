fcf31c5e7c9ecaccf2d748e8ed7a5261a39169af
victoriadillman <victoria.dillman@gmail.com>
2023-03-13 13:07:37 -0400

    got rendering logic down

--- App.jsx
+++ App.jsx
@@ -2,48 +2,40 @@
 import React, { useEffect } from "react";
 import MapDisplay from "./client/features/MapDisplay";
 import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
 // import Counter from "./client/features/counter/Counter"
 import LoginPage from "./client/features/loginPage/LoginPage"
+import { pageRender } from "./client/features/loginPage/loginPageSlice"
 
 
 function App() {
+  const dispatch = useDispatch();
 	// Going to do useEffect so that, on render, it pulls the information on which page to go to
 	useEffect(() => {
-		axios.get('http://localhost:3000/original')
+		axios.get("http://localhost:3000/original")
 			.then((data) => {
-				if (data.data.location === 'signup') {
-					return (
-						<>
-							<div id='app'>
-								SIGNUP APP LOADED
-								{/* <Counter /> */}
-								{/* <LoginPage /> */}
-								<MapDisplay />
-							</div>
-						</>
-					)
-				}
-				else {
-					return (
-            <>
-              <div id='app'>
-                MAIN APP LOADED
-                {/* <Counter /> */}
-                {/* <LoginPage /> */}
-              <MapDisplay />
-              </div>
-            </>
-          )
-				}
-			})
+        // data.data.location
+        dispatch(pageRender(data.data.location))
+      })
 	}, [])
-	return (
+  const whichComp = useSelector((state) => state.login.componentRender)
+  console.log(whichComp)
+  const renderComp = [];
+  if (whichComp === 'LoginPage') {
+    renderComp.push(< LoginPage/>)
+  }
+  else {
+    renderComp.push(< MapDisplay/>)
+  }
+  
 
+	return (
 		<>
 			<div id='app'>
 				REACT APP LOADED
-				<LoginPage />
-				<MapDisplay />
+        {renderComp}
+				{/* <LoginPage /> */}
+				{/* <MapDisplay /> */}
 			</div>
 		</>
 	);
